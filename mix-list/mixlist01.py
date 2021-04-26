b8c3c0651926808279da4d5cd21b8616dc14b9fa
SsaaPard <SsaaPard@yahoo.com>
2021-04-26 18:38:23 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+Options_list= ["ADE","ADH","AD6","AEK","AJ1","ALP","AST","CLN","CMD","GXD","MM3","STJ","WGR"]
+print("My first option in the car: " + Options_list[0] )
+print("My 2nd Option in the car: " + str(Options_list[1]) )
+print("The last item in the list (state): " + Options_list[-1] )
+
+
+        
+
